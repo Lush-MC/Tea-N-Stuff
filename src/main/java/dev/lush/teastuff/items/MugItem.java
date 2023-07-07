122d7d55e59f171f3f4c7e45659c2805b28a51a1
Luna <contact@rigi.dev>
2023-07-07 23:45:41 +0200

    texture

--- src/main/java/dev/lush/teastuff/items/MugItem.java
+++ src/main/java/dev/lush/teastuff/items/MugItem.java
@@ -2,8 +2,8 @@ package dev.lush.teastuff.items;
 
 import net.minecraft.item.Item;
 
-public class Mug extends Item {
-    public Mug(Settings settings) {
+public class MugItem extends Item {
+    public MugItem(Settings settings) {
         super(settings);
     }
 }
